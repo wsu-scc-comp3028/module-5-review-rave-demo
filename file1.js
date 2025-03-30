100528e826d1dd30efd86a14150749030d5542d3
T-CodeMaster <t.aldalain@city.westernsydney.edu.au>
2025-03-30 06:18:36 +0000

    Code commments added to the code. The code changed to async/await syntax...

--- file1.js
+++ file1.js
@@ -1,10 +1,26 @@
-const fs = require('fs');
+import fs from 'fs/promises';
 
-function readfile(filepath) {
-    fs.readFile(filepath, 'utf8', (err, data) => {
-        if (err) throw err; 
-        console.log(data); 
-    });
+/**
+ * Asynchronously reads data from the specified file path and returns the data.
+ * @param {string} filePath - The path to the file to be read.
+ * @returns {Promise<string>} The content of the file as a string.
+ */
+async function readFileAsync(filePath) {
+    try {
+        const data = await fs.readFile(filePath, { encoding: 'utf8' });
+        return data;
+    } catch (error) {
+        console.error('Failed to read file:', error);
+        throw error; // Re-throwing the error for further handling if necessary
+    }
 }
-
-readfile('./example.txt');
+// Calling the function from a name async function for demontration
+async function performFileRead() {
+    try { 
+        const data = await readFileAsync('./example.txt');
+        // do something with the data - store it for longer
+    } catch (error) {
+        console.error('Error encounted:', error);
+    }
+}
+performFileRead();
